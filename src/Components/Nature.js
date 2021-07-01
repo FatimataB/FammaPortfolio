338b318f570c9992a1c9b9e407a9787cd65fa5f5
FatimataB <fatimatab@hotmail.fr>
2021-07-01 16:31:29 +0200

    nature update

--- src/Components/Nature.js
+++ src/Components/Nature.js
@@ -21,13 +21,8 @@ const Nature =()=> {
         
 
         <section className="images">
-            <video className="image1" width="320" height="400" controls >
-                <source poster="https://res.cloudinary.com/dotv9gsm3/image/upload/v1624019540/Illustration_Nature1_byvpjc.jpg" src="https://res.cloudinary.com/dotv9gsm3/video/upload/v1624815459/video-output-E48E942C-948A-48FE-AB54-59DD2C55B9F1_2_tev2nx.mp4" type="video/mp4"/>
-                
-                Your browser does not support the video tag.
-            </video> 
-        
-             
+            <img className="papillon" src="https://acegif.com/wp-content/uploads/butterfly-animation-14.gif"/>
+            <img className="image1" src={Illustration_Nature1}/>
             {filteredIllustrations.map ((dessin)=>{
                 
                 return <Illustrations imageAddress={dessin.path}/> 
--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,9 +1,21 @@
+import shop_image1 from'../Assets/shop_image1.png'
+
+
 const Shop =()=> {
+   
     return(
-       
-      
+          
      <div className="shop">
-        coming soon
+         <section className="title">
+            <h1>E-SHOP</h1>
+            <p>COMING SOON</p>
+         </section>
+
+         <section className="shopimg">
+            <img src={shop_image1}/>
+                
+           
+         </section>
      </div>
 
 
